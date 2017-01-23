fc2a114c017fa4cb2d6684383cdb14095654a8f9
Alberto Mardomingo <alberto.mardomingo@gmail.com>
2017-01-23 08:10:57 +0100

    Fix typo in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ RSYSLOG_CONFIG="${RSYSLOG_CONFIG:-/etc/rsyslog.conf}"
 ELASTIC_HOST="${ELASTIC_HOST:-elastic}"
 ELASTIC_PORT="${ELASTIC_PORT:-9200}"
 ELASTIC_INDEX="${ELASTIC_INDEX:-rsyslogIndex}"
-ELASTIC_TYPE="${ELASTIC_TUPE:-test-type}"
+ELASTIC_TYPE="${ELASTIC_TYPE:-test-type}"
 echo "done!"
 
 echo -n "Setting values in configuration..."
